fcb6daf9c036900a64e23d8348228dc50545e3d2
Reis Bashaija <85592767+lrdbash@users.noreply.github.com>
2024-04-29 12:03:51 +0300

    Pic changes

--- loader.js
+++ loader.js
@@ -1,5 +1,5 @@
 'use client'
 
 export default function myImageLoader({src, width, quality}) {
-    return `https://nextjsportfolio.com${src}?auto=compress&cs=tinysrgb&dpr=2&h=${width}&w=${width}&q=${quality || 75}`
+    return `${src}?auto=compress&cs=tinysrgb&dpr=2&h=${width}&w=${width}&q=${quality || 75}`
 }
